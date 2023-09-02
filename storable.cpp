70b20e3ff1748954265ae879df32b9148bbf4cc6
buzz_404 <nitkkree@gmail.com>
2023-09-02 11:03:11 +0530

    Added Detailed info of storable.cpp

--- storable.cpp
+++ storable.cpp
@@ -10,14 +10,32 @@
 #include <iostream>
 
 using namespace std;
+/*Storable is a class which can be stored in a file. 
+Anything which is Storable must have a recordId(unique Key).
+derived from serializable i.e. if anything is storable must be serializable.
+(must have to_string() method)*/
  class Storable : public serializable
  {
     protected:
+        //unique recordId , acts as primary key to find data
+        /*this property(recordId) is protected . so only derived
+        class member can access it directly */
         long recordId;
     public:
+        /*constructor function , i/p is recordId*/
         Storable(long recordId) {this->recordId = recordId;}
         long getrecId() const{return this->recordId;}
+        /*Pure Virtual function.
+        Every Subclass of this class must provide a suitable way to set 
+        this object from Storable *s*/
+        //similar to copy constructor
         virtual void setDataFrom(Storable *s) = 0;
+        /*Friend's Declaration*/
+        /*Table and database can access private members of this class*/
+        /*Table class : It sets 'recordId' of object inside 'addNewRecord' method
+        */
+        /*Database class : It sets 'recordId' of object inside 'addNewRecord' method
+        */
         template<class T> friend class Table;
         friend class Database;
  };
