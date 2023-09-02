bb2593846f8e02a4b32b341232da1275172ca1c0
buzz_404 <nitkkree@gmail.com>
2023-09-02 10:42:51 +0530

    Added Detailed info of serializable.cpp

--- serializable.cpp
+++ serializable.cpp
@@ -1,10 +1,16 @@
+//Included standard CPP headers
 #include <iostream>
 #include <bits/stdc++.h>
 #include <vector>
 #include <string>
 using namespace std;
+/*This(serializable) is a abstract class and any class derived from this class 
+must implement to_string() function*/
 class serializable
 {
     public:
+    /*to_string() is a pure virtual function and serializable class don't know what 
+    this method will do but it's the responsiblity of derived classes 
+    to implement this in their own way*/
         virtual string to_string() = 0;
 };
